9b74dff342d7a7fdf7ce813523daa476959ff54a
ObedNdayizera <obedndayizera@gmail.com>
2024-08-11 16:23:50 +0200

    updated sql query import file

--- ims.sql
+++ ims.sql
@@ -11,6 +11,8 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+-- Select the database
+USE `ims`;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,32 +20,15 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ims`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `categorie`
 --
 
 CREATE TABLE `categorie` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `categorie_name` varchar(255) DEFAULT NULL,
-  `user_name` varchar(255) DEFAULT NULL
+  `user_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `categorie`
---
-
-INSERT INTO `categorie` (`id`, `categorie_name`, `user_name`) VALUES
-(2, 'food', 'john'),
-(3, 'furniture', 'rango'),
-(5, 'food', 'john'),
-(6, 'food', 'rango'),
-(23, 'furniture', 'rango'),
-(24, 'furniture', 'rango');
 
 -- --------------------------------------------------------
 
@@ -52,20 +37,12 @@ INSERT INTO `categorie` (`id`, `categorie_name`, `user_name`) VALUES
 --
 
 CREATE TABLE `product` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_name` varchar(255) DEFAULT NULL,
   `product_categorie` varchar(255) DEFAULT NULL,
-  `date` date DEFAULT NULL
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `product`
---
-
-INSERT INTO `product` (`id`, `product_name`, `product_categorie`, `date`) VALUES
-(28, 'vocado', 'food', '2024-06-05'),
-(29, 'jordan 1', 'clothes', '2024-06-10'),
-(30, 'banana', 'food', '2024-06-17');
 
 -- --------------------------------------------------------
 
@@ -74,22 +51,14 @@ INSERT INTO `product` (`id`, `product_name`, `product_categorie`, `date`) VALUES
 --
 
 CREATE TABLE `stock` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_name` varchar(255) DEFAULT NULL,
   `categorie_name` varchar(255) DEFAULT NULL,
   `stock_in` int(11) DEFAULT 0,
   `stock_out` int(11) DEFAULT 0,
-  `available` int(11) GENERATED ALWAYS AS (`stock_in` - `stock_out`) STORED
+  `available` int(11) GENERATED ALWAYS AS (`stock_in` - `stock_out`) STORED,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `stock`
---
-
-INSERT INTO `stock` (`id`, `product_name`, `categorie_name`, `stock_in`, `stock_out`) VALUES
-(5, 'vocado', 'food', 20, 8),
-(6, 'jordan 1', 'clothes', 40, 10),
-(7, 'banana', 'food', 50, 5);
 
 -- --------------------------------------------------------
 
@@ -98,78 +67,13 @@ INSERT INTO `stock` (`id`, `product_name`, `categorie_name`, `stock_in`, `stock_
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) DEFAULT NULL,
   `user_password` varchar(255) DEFAULT NULL,
-  `user_position` varchar(255) DEFAULT NULL
+  `user_position` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `user_name`, `user_password`, `user_position`) VALUES
-(3, 'rango', '$2y$10$i7QGirAvPuZPWMqFWYtiOuGPou/.lsWDAeLVsi/0W492IQf8knqIG', 'DEVELOPER'),
-(4, 'john', '$2y$10$gQdtFLNJavMve1mS67s0Rudd7dya1iSUSBKKLP1fNXatIZMfnLoaO', 'ceo'),
-(5, 'admin', '$2y$10$Pk5uQeVwRp8BtBcBI8oVyOklV.J1YX3pG797066C9Q5l5nJ0TxeHG', 'ceo'),
-(6, 'ObedNdayizera', 'DEVELOPER', '1234'),
-(8, 'vava', '$2y$10$5JH4BjmHSEDsippWnuUSzea04cTLqz8tGorxgFmAbW.ToKPForiLG', 'ceo');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `stock`
---
-ALTER TABLE `stock`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categorie`
---
-ALTER TABLE `categorie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
-
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-
---
--- AUTO_INCREMENT for table `stock`
---
-ALTER TABLE `stock`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
